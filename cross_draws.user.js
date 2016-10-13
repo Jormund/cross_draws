a2f2a24167d6abf5f2354634f5618df110e2c648
Jormund <etiennebotek@gmail.com>
2016-10-13 20:34:22 +0200

    Handle https://ingress.com/intel

--- cross_draws.user.js
+++ cross_draws.user.js
@@ -2,18 +2,16 @@
 // @id             iitc-plugin-cross-draws@Jormund
 // @name           IITC plugin: cross draws
 // @category       Layer
-// @version        1.1.3.20160711.1659
+// @version        1.1.3.20161013.2028
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @downloadURL    https://raw.githubusercontent.com/Jormund/cross_draws/master/cross_draws.user.js
-// @description    [2016-07-11-1659] Checks for planned links that cross other planned links. Requires draw-tools plugin.
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
-// @include        https://www.ingress.com/mission/*
-// @include        http://www.ingress.com/mission/*
-// @match          https://www.ingress.com/mission/*
-// @match          http://www.ingress.com/mission/*
+// @description    [2016-10-13-2028] Checks for planned links that cross other planned links. Requires draw-tools plugin.
+// @include        https://ingress.com/intel*
+// @include        http://ingress.com/intel*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
 // @grant          none
 // ==/UserScript==
 
