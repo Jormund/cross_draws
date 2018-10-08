62ac8dd86b8f3b0b2b2e10a4a8c59e8a8ea04b10
Jormund <etiennebotek@gmail.com>
2018-10-08 16:39:29 +0200

    1.2.0

--- cross_draws.meta.js
+++ cross_draws.meta.js
@@ -2,11 +2,10 @@
 // @id             iitc-plugin-cross-draws@Jormund
 // @name           IITC plugin: cross draws
 // @category       Layer
-// @version        1.1.3.20180124.2001
+// @version        1.2.0.20181008.1630
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @updateURL      https://cdn.rawgit.com/Jormund/cross_draws/master/cross_draws.meta.js
-// @downloadURL    https://cdn.rawgit.com/Jormund/cross_draws/master/cross_draws.user.js
-// @description    [2016-10-13-2028] Checks for planned links that cross other planned links. Requires draw-tools plugin.
+// @downloadURL    https://raw.githubusercontent.com/Jormund/cross_draws/master/cross_draws.user.js
+// @description    [2018-10-08-1630] Checks for planned links that cross other planned links. Requires draw-tools plugin.
 // @include        https://ingress.com/intel*
 // @include        http://ingress.com/intel*
 // @include        https://*.ingress.com/intel*
