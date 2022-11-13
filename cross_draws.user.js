d7d3ecd6d9d0f60739087ec3b5e1291905911769
Jormund <etiennebotek@gmail.com>
2022-11-13 23:07:40 +0100

    Activate on intel-x.ingress.com

--- cross_draws.user.js
+++ cross_draws.user.js
@@ -2,14 +2,13 @@
 // @id             iitc-plugin-cross-draws@Jormund
 // @name           IITC plugin: cross draws
 // @category       Layer
-// @version        1.2.3.20221113.2221
+// @version        1.2.3.20221113.2222
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://raw.githubusercontent.com/Jormund/cross_draws/master/cross_draws.meta.js
 // @downloadURL    https://raw.githubusercontent.com/Jormund/cross_draws/master/cross_draws.user.js
-// @description    [2022-11-13-2221] Checks for planned links that cross other planned links. Requires draw-tools plugin.
-// @include        https://*.ingress.com/intel*
-// @include        https://intel.ingress.com/*
+// @description    [2022-11-13-2222] Checks for planned links that cross other planned links. Requires draw-tools plugin.
 // @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
 // @match          https://*.ingress.com/intel*
 // @grant          none
 // ==/UserScript==
